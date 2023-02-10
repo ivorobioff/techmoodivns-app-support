78eac085b9b1cbca0cfe502667c8277aba39a3b1
Igor Vorobiov <igor.vorobioff@gmail.com>
2023-02-10 14:27:39 +0200

    some updates

--- src/main/java/eu/techmoodivns/support/security/SecurityConfigurer.java
+++ src/main/java/eu/techmoodivns/support/security/SecurityConfigurer.java
@@ -2,65 +2,57 @@ package eu.techmoodivns.support.security;
 
 import eu.techmoodivns.support.cors.CorsConfiguration;
 import eu.techmoodivns.support.security.preference.SecurityPreference;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.http.HttpMethod;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.stereotype.Component;
-
-import java.util.Map;
-import java.util.Set;
-
 import static eu.techmoodivns.support.security.controller.AuthController.*;
 import static java.util.stream.Collectors.*;
 
 @Component
 public class SecurityConfigurer {
 
-    @Autowired
-    private SecurityPreference securityPreference;
+    @Bean
+    AuthenticationManager configureAuthenticationManager(
+            HttpSecurity http,
+            AuthenticationManagerBuilder authenticationManagerBuilder,
+            SecretAuthenticationProvider authenticationProvider) throws Exception {
 
-    @Autowired
-    private ApplicationContext applicationContext;
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-        SecretAuthenticationProvider provider = getApplicationContext()
-                .getAutowireCapableBeanFactory()
-                .createBean(SecretAuthenticationProvider.class);
-
-        auth.authenticationProvider(provider);
+        return authenticationManagerBuilder.authenticationProvider(authenticationProvider).build();
     }
 
-    @Override
-    public void configure(WebSecurity web) {
-        Map<HttpMethod, Set<String>> openEndpoints = securityPreference.getOpenEndpoints()
-                .stream()
-                .collect(groupingBy(SecurityPreference.Endpoint::getMethod, mapping(SecurityPreference.Endpoint::getPath, toSet())));
+    @Bean
+    WebSecurityCustomizer configureWebSecurity(SecurityPreference securityPreference) throws Exception {
+        return (WebSecurity web) -> {
+            var openEndpoints = securityPreference.getOpenEndpoints()
+                    .stream()
+                    .collect(groupingBy(SecurityPreference.Endpoint::getMethod,
+                            mapping(SecurityPreference.Endpoint::getPath, toSet())));
 
+            var ignoring = web.ignoring();
 
-        WebSecurity.IgnoredRequestConfigurer ignoring = web.ignoring();
+            ignoring.requestMatchers(
+                    LOGIN_ENDPOINT,
+                    LOGOUT_ENDPOINT,
+                    REFRESH_ENDPOINT);
 
-        ignoring.mvcMatchers(
-                LOGIN_ENDPOINT,
-                LOGOUT_ENDPOINT,
-                REFRESH_ENDPOINT);
-
-
-        openEndpoints.forEach((method, paths) ->
-                ignoring.mvcMatchers(method, paths.toArray(new String[0])));
+            openEndpoints.forEach((method, paths) -> ignoring.requestMatchers(method, paths.toArray(new String[0])));
+        };
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    SecurityFilterChain configureSecurityFilterChain(
+        HttpSecurity http, 
+        ApplicationContext applicationContext) throws Exception {
 
-        http.requestMatcher(new AntPathRequestMatcher("/api/v1.0/**"));
+        http.securityMatcher("/api/v1.0/**");
 
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
@@ -70,8 +62,10 @@ public class SecurityConfigurer {
             http.cors();
         }
 
-        http.authorizeRequests().antMatchers("/**").authenticated();
+        http.authorizeHttpRequests().requestMatchers("/**").authenticated();
 
         http.csrf().disable();
+
+        return http.build();
     }
 }
